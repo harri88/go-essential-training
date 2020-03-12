365ab444d9dd1dc8ee6960cfabb78698b84c1ad7
Harri <harrimuhammad@gmail.com>
2020-03-12 16:05:34 +0700

    add conditional flow

--- if.go
+++ if.go
@@ -10,4 +10,13 @@ func main() {
 	if x > 5 {
 		fmt.Println("x is big")
 	}
+	if x > 100 {
+		fmt.Println("x is very big")
+	} else {
+		fmt.Println("x is not that big")
+	}
+
+	if x > 5 && x < 15 {
+		fmt.Println("x is just right")
+	}
 }
