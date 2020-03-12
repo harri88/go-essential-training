09d4a8ac05d791c91379d41146faf6024a874906
Harri <harrimuhammad@gmail.com>
2020-03-12 17:13:04 +0700

    add switch.go for Example of switch statement

---
+++ switch.go
@@ -0,0 +1,22 @@
+// Example of switch statement
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 7
+
+	switch x {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	default:
+		fmt.Println("many")
+	}
+}
